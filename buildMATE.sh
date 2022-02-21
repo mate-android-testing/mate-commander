5dfba60063392a1c43807067aaff0799e06c5847
Michael <auermich@fim.uni-passau.de>
2022-02-21 18:14:15 +0100

    adjusted build script

--- buildMATE.sh
+++ buildMATE.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# we need to re-build the MATE APKs for each target app separately
+packageName=$1
+
 # You have to adjust the paths here!!!
 MATE_HOME="C:\\Users\\Michael\\git\\mate"
 MATE_SERVER_HOME="C:\\Users\\Michael\\git\\mate-server"
@@ -8,13 +11,17 @@ MATE_COMMANDER_HOME="C:\\Users\\Michael\\git\\mate-commander"
 # build MATE APKs
 cd $MATE_HOME
 # if Java 15 is not working, use Java 11, e.g. ./gradlew -Dorg.gradle.java.home="C:\\Program Files\\Java\\jdk-11" assembleDebug
-./gradlew assembleDebug
-./gradlew assembleAndroidTest
+./gradlew :representation:assembleDebugAndroidTest -PtargetPackage=$packageName
+./gradlew :client:assembleDebug
 
 # copy MATE APKs into mate-commander
 cd $MATE_COMMANDER_HOME
-cp $MATE_HOME\\app\\build\\outputs\\apk\\androidTest\\debug\\app-debug-androidTest.apk app-debug-androidTest.apk
-cp $MATE_HOME\\app\\build\\outputs\\apk\\debug\\app-debug.apk app-debug.apk
+cp $MATE_HOME\\representation\\build\\outputs\\apk\\androidTest\\debug\\representation-debug-androidTest.apk representation-debug-androidTest.apk
+cp $MATE_HOME\\client\\build\\outputs\\apk\\debug\\client-debug.apk client-debug.apk
+
+# sign the APKs with the same key as the APK of the AUT
+./signAPK.sh representation-debug-androidTest.apk
+./signAPK.sh client-debug.apk
 
 # build MATE-Server jar
 cd $MATE_SERVER_HOME
