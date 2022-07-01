28c618b43c8059e4e0b3091f78afb59d4acc9af7
Michael Pusl <pusl@fim.uni-passau.de>
2022-07-01 08:16:19 +0200

    Resolved merge conflict

--- signAPK.sh
+++ signAPK.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# zipalign=$ANDROID_HOME/build-tools/30.0.2/zipalign
-zipalign -p 4 $1 $1.aligned
+# adjust the path accordingly!
+zipalign=$ANDROID_HOME/build-tools/31.0.0/zipalign
+apksigner=$ANDROID_HOME/build-tools/31.0.0/apksigner
 
-# there is a bug with zipalign, we need two iterations, see: https://stackoverflow.com/questions/38047358/zipalign-verification-failed-resources-arsc-bad-1
-zipalign -f -p 4 $1.aligned $1.aligned2
+$zipalign -p 4 $1 $1.aligned
 
-# zipalign doesn't work in place :(
+# There is a bug with zipalign, we need two iterations.
+# See: https://stackoverflow.com/questions/38047358/zipalign-verification-failed-resources-arsc-bad-1
+$zipalign -f -p 4 $1.aligned $1.aligned2
+
 rm $1.aligned
 mv $1.aligned2 $1
 
-# apksigner=$ANDROID_HOME/build-tools/30.0.2/apksigner.bat
-apksigner sign --ks ~/.android/debug.keystore --ks-key-alias androiddebugkey --ks-pass pass:android --key-pass pass:android $1
+$apksigner sign --ks ~/.android/debug.keystore --ks-key-alias androiddebugkey --ks-pass pass:android --key-pass pass:android $1
+
 # alternatively create your own keystore: https://stackoverflow.com/questions/3997748/how-can-i-create-a-keystore
 # apksigner sign --ks "C:\Program Files\Java\jdk1.8.0_131\jre\bin\KeyStore.jks" --ks-key-alias "mydomain" --ks-pass pass:123456  $1
 
 # remove generated signature file
 rm $1.idsig
-
-# jarsigner is outdated
-# jarsigner -verbose -storepass 123456 -keystore "C:\Program Files\Java\jdk1.8.0_131\jre\bin\KeyStore.jks" $1 "mydomain"
